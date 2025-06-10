0895d169ef7feba6ef8902df6fd72400e7edad85
Rashmi Shastri <rashmi.shastri.22@gmail.com>
2025-06-10 11:08:13 +0530

    Updated UserService

--- backend/src/main/java/io/inkHeart/service/UserService.java
+++ backend/src/main/java/io/inkHeart/service/UserService.java
@@ -1,48 +1,16 @@
 package io.inkHeart.service;
 
-import com.nimbusds.srp6.SRP6CryptoParams;
-import com.nimbusds.srp6.SRP6ServerSession;
 import io.inkHeart.entity.User;
 import io.inkHeart.repository.UserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigInteger;
-import java.util.Base64;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final Map<String, SRP6ServerSession> srpSessions;
-    public Map<String, SRP6ServerSession> getSrpSessions() {
-        return srpSessions;
-    }
-
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.srpSessions = new ConcurrentHashMap<>();
     }
 
-
-    public SRP6ServerSession startLoginChallenge(String email) {
-        var user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Email not found"));
-
-        SRP6CryptoParams cryptoParams = SRP6CryptoParams.getInstance(2048, "SHA-1"); // Is SHA-1 right?
-        return new SRP6ServerSession(cryptoParams);
-    }
-    public User login(String email, String password) {
-        // what about error code?
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Invalid user credentials"));
-//        if (!Argon2Utils.verifyPassword(user.getPasswordHash(), password)) {
-//            throw new RuntimeException("Invalid password");
-//        }
-        return user;
-    }
 
     public void deleteUserById(Long id) {
         userRepository.deleteById(id);
